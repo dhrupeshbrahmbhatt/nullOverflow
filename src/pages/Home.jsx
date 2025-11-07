21755606bab15a0af457321512efcd3219807ddd
am314-web <akmy1624@gmail.com>
2025-11-08 00:47:04 +0530

    improved hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -167,12 +167,12 @@ const Home = () => {
                     <span>View works</span>
                   </a>
 
-                  <div className="mil-circle-text">
+                  <div className="mil-circle-text" style={{ marginRight: '300px', transform: 'scale(0.7)' }}>
                     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 300 300" enableBackground="new 0 0 300 300" xmlSpace="preserve" className="mil-ct-svg mil-rotate" data-value="360">
                       <defs>
                         <path id="circlePath" d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "></path>
                       </defs>
-                      <circle cx="150" cy="100" r="75" fill="none"></circle>
+                      <circle cx="60" cy="80" r="45" fill="none"></circle>
                       <g>
                         <use xlinkHref="#circlePath" fill="none"></use>
                         <text style={{letterSpacing: '6.5px'}}>
