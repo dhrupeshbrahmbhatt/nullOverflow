bd2f01a3e9bf522c0fdb71de000b460f41c23fc3
am314-web <akmy1624@gmail.com>
2025-11-08 00:48:24 +0530

    improved hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,22 @@ const Home = () => {
       </div>
 
      {/* {/* Preloader */}
-      
+      <div className="mil-preloader">
+        <div className="mil-preloader-animation">
+          <div className="mil-pos-abs mil-animation-1">
+            <p className="mil-h3 mil-muted mil-thin">Pioneering</p>
+            <p className="mil-h3 mil-muted">Creative</p>
+            <p className="mil-h3 mil-muted mil-thin">Excellence</p>
+          </div>
+          <div className="mil-pos-abs mil-animation-2">
+            <div className="mil-reveal-frame">
+              <p className="mil-reveal-box"></p>
+              <p className="mil-h3 mil-muted mil-thin">∅verflow()</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
 
       {/* Scrollbar Progress */}
