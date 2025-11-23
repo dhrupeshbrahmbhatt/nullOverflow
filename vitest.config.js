6857427c5528ca66dbf9d280081aafd73c720e90
priteshbarot <37903393+priteshbarot@users.noreply.github.com>
2025-11-23 19:18:24 +0530

    Add pool configuration to Vitest setupvit

--- vitest.config.js
+++ vitest.config.js
@@ -7,5 +7,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
   },
 });
